4a8f3fcb028368d7486f737d5b1103e9790a7fb0
willsonluo <Willson_luo@163.com>
2021-10-15 22:26:56 +0800

    update.

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -55,3 +55,32 @@ resource "aws_route" "public" {
   gateway_id = aws_internet_gateway.gw.id
 }
 
+resource "aws_network_acl" "main" {
+  vpc_id = aws_vpc.main.id
+
+  egress = [
+    {
+      protocol   = "tcp"
+      rule_no    = 200
+      action     = "allow"
+      cidr_block = "192.168.0.0/16"
+      from_port  = 443
+      to_port    = 443
+    }
+  ]
+
+  ingress = [
+    {
+      protocol   = "tcp"
+      rule_no    = 100
+      action     = "allow"
+      cidr_block = "192.168.0.0/16"
+      from_port  = 80
+      to_port    = 80
+    }
+  ]
+
+  tags = {
+    Name = "main"
+  }
+}
